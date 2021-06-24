7cf62e006b99ee78cb0e0d1daaf021891af8f56b
Naveenbittu <85916508+Naveenbittu@users.noreply.github.com>
2021-06-24 22:27:26 +0530

    Add files via upload

---
+++ string_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+
+
+    char ch[50] = "Naveen bittu";
+
+
+    printf("Entered String is:");
+
+    int len
+        = printf("%s\n", ch);
+
+    printf("Length is:");
+
+
+    printf("%d", len - 1);
+}
+#if 0
+Entered String is:Naveen bittu
+Length is:13
+#endif
+
